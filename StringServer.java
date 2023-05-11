896f6804c09ae9ba7a9a66c39eed9facc3c62600
eleanormng <eleanormng@gmail.com>
2023-05-10 21:10:57 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -1,33 +1,40 @@
 import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 class Handler implements URLHandler {
-    List<String> lines;
-    String path;
-    StringHandler(String path) throws IOException {
-        this.path = path;
-        this.lines = Files.readAllLines(Paths.get(path));
-    
-    
-    public String handleRequest(URI url) {
+    String message = "";
+
+    public String handleRequest(URI url) throws IOException {
+        String query = url.getQuery();
         if (url.getPath().contains("/add-message")) {
-            String[] parameters = url.getQuery().split("=");
-            if (parameters[0].equals("s")) {
-                return String.format("\n + parameters[1]");
+            if (query.startsWith("s=")) {
+                String toAdd = query.split("=")[1];
+                this.message.add(toAdd);
+                return String.format(message + "\n");
             }
+        }
+        else {
+            return "/add-message requires a query parameter s\n";
         }
     }
 }
 
-class NumberServer {
-    public static void main(String[] args) throws IOException {
-        if(args.length == 0){
-            System.out.println("Missing port number! Try any number between 1024 to 49151");
-            return;
-        }
-
-        int port = Integer.parseInt(args[0]);
-
-        Server.start(port, new Handler());
+class StringServer {
+  public static void main(String[] args) throws IOException {
+    if(args.length == 0){
+      System.out.println("Missing port number! Try any number between 1024 to 49151");
+      return;
     }
+    if(args.length == 1){
+      System.out.println("Missing file path! Give a path to a text file as the second argument.");
+      return;
+    }
+
+    int port = Integer.parseInt(args[0]);
+
+    Server.start(port, new StringHandler(args[1]));
+  }
 }
